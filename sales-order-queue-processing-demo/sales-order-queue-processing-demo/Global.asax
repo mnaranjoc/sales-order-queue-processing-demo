c98b7a85f94e8c49a4b3b08284039063ea43c514
manuel.naranjo <manuel.naranjo@MANUELNARANJO>
2019-06-07 11:02:55 -0500

    Project created

---
+++ sales-order-queue-processing-demo/sales-order-queue-processing-demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sales_order_queue_processing_demo.MvcApplication" Language="C#" %>
---
+++ sales-order-queue-processing-demo/sales-order-queue-processing-demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sales_order_queue_processing_demo.Startup))]
+namespace sales_order_queue_processing_demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
