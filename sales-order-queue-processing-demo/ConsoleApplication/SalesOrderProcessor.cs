1ef09139515ff17d3bd0a7b005d11b898a7235a2
Manuel Naranjo <manuelnaranjo@outlook.com>
2019-10-24 23:55:05 -0500

    Poject layout

---
+++ sales-order-queue-processing-demo/ConsoleApplication/SalesOrderProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class SalesOrderProcessor
+    {
+        public void prompt()
+        {
+            Console.WriteLine("");
+        }
+
+        public bool validate()
+        {
+            bool ret = true;
+
+            return ret;
+        }
+
+        public void run()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            SalesOrderProcessor processor;
+
+            processor = new SalesOrderProcessor();
+            processor.prompt();
+
+            if (processor.validate())
+            {
+                processor.run();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
