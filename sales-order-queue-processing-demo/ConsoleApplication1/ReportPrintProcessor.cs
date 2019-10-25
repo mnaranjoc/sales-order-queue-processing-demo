60c6115ee9f1693852a89857145efdfd37a1f46f
Manuel Naranjo <manuelnaranjo@outlook.com>
2019-10-25 00:08:10 -0500

    Report print processor

---
+++ sales-order-queue-processing-demo/ConsoleApplication1/ReportPrintProcessor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class ReportPrintProcessor
+    {
+        public void prompt()
+        {
+
+        }
+
+        public bool validate()
+        {
+            bool ret = true;
+
+            return ret;
+        }
+
+        public void run()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            ReportPrintProcessor processor;
+
+            processor = new ReportPrintProcessor();
+            processor.prompt();
+
+            if (processor.validate())
+            {
+                processor.run();
+            }
+        }
+    }
+}
